f5f41f946d37b548bdad4250271e9b2e30e36924
Nagul75 <nagulk697@gmail.com>
2026-03-07 21:53:45 +0530

    CS-7 wrap react-query and theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,21 @@ import { createRoot } from "react-dom/client"
 
 import "./index.css"
 
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import { ThemeProvider } from "next-themes";
+
 import { Routes } from "./Routes"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 const router = createBrowserRouter(Routes);
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <ThemeProvider attribute="class" defaultTheme="system">
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router}/>
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>
 )

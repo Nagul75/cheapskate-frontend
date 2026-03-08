b254b2c171aeeb3ab0ccea1acc14dcc402cf0bb0
Nagul75 <nagulk697@gmail.com>
2026-03-08 13:10:41 +0530

    remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,6 @@ const router = createBrowserRouter(Routes);
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
     <AuthProvider>
       <ThemeProvider attribute="class" defaultTheme="system">
         <QueryClientProvider client={queryClient}>
@@ -22,5 +21,4 @@ createRoot(document.getElementById("root")!).render(
         </QueryClientProvider>
       </ThemeProvider>
     </AuthProvider>
-  </StrictMode>
 )

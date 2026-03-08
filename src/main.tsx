f8aaef9bcb82da2936c8d9b994f405862e34e7d3
Nagul75 <nagulk697@gmail.com>
2026-03-08 10:50:25 +0530

    CS-6 wrap authProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css"
 
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import { ThemeProvider } from "next-themes";
+import { AuthProvider } from "./providers/authProvider";
 
 import { Routes } from "./Routes"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
@@ -14,10 +15,12 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider attribute="class" defaultTheme="system">
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router}/>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <AuthProvider>
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router}/>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </AuthProvider>
   </StrictMode>
 )

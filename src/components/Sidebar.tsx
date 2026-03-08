f6080b21efa410fcb30c7aa1564a11a69e45f16d
Nagul75 <nagulk697@gmail.com>
2026-03-08 09:53:35 +0530

    add sidebar and header with theme toggle

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,53 @@
+import {
+  Sidebar,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarContent,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+} from "@/components/ui/sidebar";
+
+import { Wallet, Settings } from "lucide-react";
+
+function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar {...props}>
+      <SidebarHeader>
+        <div className="flex items-center gap-2">
+          <Wallet size={24} className="text-pink-600" />
+          <h1 className="text-xl font-mono font-medium tracking-tighter first:mt-0">
+            Cheapskate
+          </h1>
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton className="font-light">Dashboard</SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton className="font-light">Transactions</SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton className="font-light">Accounts</SidebarMenuButton>
+              </SidebarMenuItem>
+              <SidebarMenuItem>
+                <SidebarMenuButton className="font-light">Categories</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter className="border-t p-4">
+            <Settings/>
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
+
+export default AppSidebar;

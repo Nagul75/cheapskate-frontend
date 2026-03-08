674b25a35a6b3281fe037af4497092342c4fcc47
Nagul75 <nagulk697@gmail.com>
2026-03-08 10:48:57 +0530

    CS-6 add login and protected route to router

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,15 @@
 import {type RouteObject} from "react-router-dom";
 import Layout from "./components/Layout";
+import { Login } from "./pages/Login";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 export const Routes:RouteObject[] = [
     {
+        path: "/login",
+        element: <Login/>
+    },
+    {
         path: "/",
-        element: <Layout/>
+        element: <ProtectedRoute><Layout/></ProtectedRoute>
     }
 ]
